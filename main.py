2732eeaf8637ec971a1052d68a6c7873f21f1865
kahenoz <kahenozz@gmail.com>
2024-09-13 21:46:12 +0200

    send email is turned into a function and imported in main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 key = "049137c2a49c4436b95a85ed73dc587b"
 url = "https://newsapi.org/v2/everything?q=apple&from="\
@@ -13,9 +14,10 @@ content = request.json()
 
 #source, author, title, description, url, urlToImage, publishedAt, content, __len__
 #Access the article data
+body = ""
 for article in content["articles"]:
-      print(article['title'])
-      print(article['description'])
+      if article["title"] in not None:
+            body = body + article["title"] + "\n" + article["description"] + 2*"\n"
 
-
-
+body = body.encode("utf-8")
+send_email(message=body)
--- send_email.py
+++ send_email.py
@@ -1,21 +1,16 @@
 import smtplib, ssl
 import os
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "kahenozturk@gmail.com"
-password = os.getenv("APP_PASSWORD")
+    username = "kahenozturk@gmail.com"
+    password = os.getenv("APP_PASSWORD")
 
-receiver = "kahenozturk@gmail.com"
-context = ssl.create_default_context()
+    receiver = "kahenozturk@gmail.com"
+    context = ssl.create_default_context()
 
-message = """
-Hi!
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
-
-
-
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
